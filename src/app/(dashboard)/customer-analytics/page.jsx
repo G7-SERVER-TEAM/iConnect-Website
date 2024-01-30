8e6ea7de964fd3f4013d674b93c3c4409985a0c3
Muuwu <wasawat.csm@gmail.com>
2024-01-30 23:37:09 +0700

    Project Initialize
    create Mock-up Login Page,
    create prototype Sidebar component,
    construct a route to different pages

---
+++ src/app/(dashboard)/customer-analytics/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CustomerAnalytics = () => {
+  return (
+    <div>CustomerAnalytics</div>
+  )
+}
+
+export default CustomerAnalytics
---
+++ src/app/(dashboard)/price-config/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PriceConfig = () => {
+  return (
+    <div>PriceConfig</div>
+  )
+}
+
+export default PriceConfig
