8ab52b1dd969de9c90821f9dbf44361727431809
imwm-ns <narathip_aot@hotmail.com>
2024-03-27 19:02:32 +0700

    Integrate source code with api.

--- src/components/Container.jsx
+++
@@ -1,7 +0,0 @@
-export default function Container({children}) {
-  return (
-    <div className="lg:container lg:mx-auto px-3 2xl:px-36">
-      {children}  
-    </div>
-  );
-};
--- src/components/PageTitle.jsx
+++
@@ -1,7 +0,0 @@
-export default function PageTitle({children}) {
-  return (
-    <div className="w-max font-semibold text-2xl xl:text-3xl">
-      {children}  
-    </div>
-  );
-};
